570f4b30493051745b0286c5b3e601b4167df8d5
Jared Howland <github@jaredhowland.com>
2013-06-09 15:16:54 -0600

    [Fix] Documentation

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ This is a database that can ingest [Project COUNTER][] Book Report #1 and Book R
 
 1. Download [source files][].
 2. Untar files to desired location on your server.
-3. Create a MySQL database called `book_usage` and give desired user access to this new database.
-4. Run the SQL commands found [here](sql.md) to create tables and views needed for this application.
+3. Create a MySQL database called `book_usage` and give a user access to this new database.
+4. Run the SQL commands found [here](documentation/schema.sql) to create tables and views needed for this application.
 5. Change name of config_example.php to config.php
 6. Enter your database credentials in the config.php file
-7. Set up a cron job on your server to the following command at desired interval (every other minute or so is usually a good start):
+7. Set up a cron job to regularly run the following command (every other minute is the suggested default):
         ```php -q /path/to/process.php >/dev/null 2>&1```
-8. Adjust [Twig templates][] in the templates directory for desired HTML output.
+8. Adjust the [Twig templates][] in the [templates directory](templates) to your liking.
 
 [Project COUNTER]: http://www.projectcounter.org/
 [source files]: https://github.com/jaredhowland/Ebook-Usage-Database/archive/v1.1.tar.gz
--- documentation/schema.sql
+++ documentation/schema.sql
@@ -1,15 +1,11 @@
-```sql
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `book_usage` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
-USE `book_usage` ;
-
 -- -----------------------------------------------------
--- Table `book_usage`.`books`
+-- Table `books`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_usage`.`books` (
+CREATE TABLE IF NOT EXISTS `books` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `title` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL ,
   `author` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL ,
@@ -28,9 +24,9 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `book_usage`.`vendors`
+-- Table `vendors`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_usage`.`vendors` (
+CREATE TABLE IF NOT EXISTS `vendors` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `vendor` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL ,
   PRIMARY KEY (`id`) )
@@ -40,9 +36,9 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `book_usage`.`platforms`
+-- Table `platforms`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_usage`.`platforms` (
+CREATE TABLE IF NOT EXISTS `platforms` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `vendor_id` INT(11) NOT NULL ,
   `platform` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL ,
@@ -50,7 +46,7 @@ CREATE  TABLE IF NOT EXISTS `book_usage`.`platforms` (
   INDEX `vendor_id` (`vendor_id` ASC) ,
   CONSTRAINT `platforms_vendor_id`
     FOREIGN KEY (`vendor_id` )
-    REFERENCES `book_usage`.`vendors` (`id` )
+    REFERENCES `vendors` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -59,9 +55,9 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `book_usage`.`books_platforms`
+-- Table `books_platforms`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_usage`.`books_platforms` (
+CREATE TABLE IF NOT EXISTS `books_platforms` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `book_id` INT(11) NOT NULL ,
   `platform_id` INT(11) NOT NULL ,
@@ -70,12 +66,12 @@ CREATE  TABLE IF NOT EXISTS `book_usage`.`books_platforms` (
   INDEX `platform_id` (`platform_id` ASC) ,
   CONSTRAINT `books_platforms_book_id`
     FOREIGN KEY (`book_id` )
-    REFERENCES `book_usage`.`books` (`id` )
+    REFERENCES `books` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `books_platforms_platform_id`
     FOREIGN KEY (`platform_id` )
-    REFERENCES `book_usage`.`platforms` (`id` )
+    REFERENCES `platforms` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -84,9 +80,9 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `book_usage`.`books_search`
+-- Table `books_search`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_usage`.`books_search` (
+CREATE TABLE IF NOT EXISTS `books_search` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `title` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL ,
   `author` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL ,
@@ -107,9 +103,9 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `book_usage`.`books_vendors`
+-- Table `books_vendors`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_usage`.`books_vendors` (
+CREATE TABLE IF NOT EXISTS `books_vendors` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `book_id` INT(11) NOT NULL ,
   `vendor_id` INT(11) NOT NULL ,
@@ -118,12 +114,12 @@ CREATE  TABLE IF NOT EXISTS `book_usage`.`books_vendors` (
   INDEX `vendor_id` (`vendor_id` ASC) ,
   CONSTRAINT `books_vendors_book_id`
     FOREIGN KEY (`book_id` )
-    REFERENCES `book_usage`.`books` (`id` )
+    REFERENCES `books` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `books_vendors_vendor_id`
     FOREIGN KEY (`vendor_id` )
-    REFERENCES `book_usage`.`vendors` (`id` )
+    REFERENCES `vendors` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -132,9 +128,9 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `book_usage`.`counter_br1`
+-- Table `counter_br1`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_usage`.`counter_br1` (
+CREATE TABLE IF NOT EXISTS `counter_br1` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `book_id` INT(11) NOT NULL ,
   `vendor_id` INT(11) NOT NULL ,
@@ -148,17 +144,17 @@ CREATE  TABLE IF NOT EXISTS `book_usage`.`counter_br1` (
   INDEX `year` (`usage_year` ASC) ,
   CONSTRAINT `counter_br1_book_id`
     FOREIGN KEY (`book_id` )
-    REFERENCES `book_usage`.`books` (`id` )
+    REFERENCES `books` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `counter_br1_platform_id`
     FOREIGN KEY (`platform_id` )
-    REFERENCES `book_usage`.`platforms` (`id` )
+    REFERENCES `platforms` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `counter_br1_vendor_id`
     FOREIGN KEY (`vendor_id` )
-    REFERENCES `book_usage`.`vendors` (`id` )
+    REFERENCES `vendors` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -167,9 +163,9 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `book_usage`.`counter_br2`
+-- Table `counter_br2`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_usage`.`counter_br2` (
+CREATE TABLE IF NOT EXISTS `counter_br2` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `book_id` INT(11) NOT NULL ,
   `vendor_id` INT(11) NOT NULL ,
@@ -183,17 +179,17 @@ CREATE  TABLE IF NOT EXISTS `book_usage`.`counter_br2` (
   INDEX `year` (`usage_year` ASC) ,
   CONSTRAINT `counter_br2_book_id`
     FOREIGN KEY (`book_id` )
-    REFERENCES `book_usage`.`books` (`id` )
+    REFERENCES `books` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `counter_br2_platform_id`
     FOREIGN KEY (`platform_id` )
-    REFERENCES `book_usage`.`platforms` (`id` )
+    REFERENCES `platforms` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `counter_br2_vendor_id`
     FOREIGN KEY (`vendor_id` )
-    REFERENCES `book_usage`.`vendors` (`id` )
+    REFERENCES `vendors` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -202,9 +198,9 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `book_usage`.`temp_counter_br1`
+-- Table `temp_counter_br1`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_usage`.`temp_counter_br1` (
+CREATE TABLE IF NOT EXISTS `temp_counter_br1` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `title` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL ,
   `publisher` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL ,
@@ -223,9 +219,9 @@ COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
--- Table `book_usage`.`temp_counter_br2`
+-- Table `temp_counter_br2`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_usage`.`temp_counter_br2` (
+CREATE TABLE IF NOT EXISTS `temp_counter_br2` (
   `id` INT(11) NOT NULL AUTO_INCREMENT ,
   `title` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL ,
   `publisher` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL ,
@@ -242,118 +238,51 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_unicode_ci;
 
-USE `book_usage` ;
+-- -----------------------------------------------------
+-- View `counter_usage`
+-- -----------------------------------------------------
+CREATE OR REPLACE VIEW `counter_usage` AS (select `counter_br1`.`book_id` AS `book_id`,`counter_br1`.`vendor_id` AS `vendor_id`,`counter_br1`.`platform_id` AS `platform_id`,`counter_br1`.`usage_year` AS `usage_year`,`counter_br1`.`counter_br1` AS `counter_usage`,'br1' AS `usage_type` from `counter_br1`) union all (select `counter_br2`.`book_id` AS `book_id`,`counter_br2`.`vendor_id` AS `vendor_id`,`counter_br2`.`platform_id` AS `platform_id`,`counter_br2`.`usage_year` AS `usage_year`,`counter_br2`.`counter_br2` AS `counter_usage`,'br2' AS `usage_type` from `counter_br2`);
 
 -- -----------------------------------------------------
--- Placeholder table for view `book_usage`.`all_usage`
+-- View `current_br1`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_usage`.`all_usage` (`id` INT, `current_br1` INT, `current_br2` INT, `previous_br1` INT, `previous_br2` INT);
+CREATE OR REPLACE VIEW `current_br1` AS select `counter_br1`.`book_id` AS `book_id`,`counter_br1`.`counter_br1` AS `counter_br1` from `counter_br1` where (`counter_br1`.`usage_year` = 2012);
 
 -- -----------------------------------------------------
--- Placeholder table for view `book_usage`.`counter_usage`
+-- View `current_br2`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_usage`.`counter_usage` (`book_id` INT, `vendor_id` INT, `platform_id` INT, `usage_year` INT, `counter_usage` INT, `usage_type` INT);
+CREATE OR REPLACE VIEW `current_br2` AS select `counter_br2`.`book_id` AS `book_id`,`counter_br2`.`counter_br2` AS `counter_br2` from `counter_br2` where (`counter_br2`.`usage_year` = 2012);
 
 -- -----------------------------------------------------
--- Placeholder table for view `book_usage`.`current_br1`
+-- View `previous_br1`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_usage`.`current_br1` (`book_id` INT, `counter_br1` INT);
+CREATE OR REPLACE VIEW `previous_br1` AS select `counter_br1`.`book_id` AS `book_id`,`counter_br1`.`counter_br1` AS `counter_br1` from `counter_br1` where (`counter_br1`.`usage_year` = 2011);
 
 -- -----------------------------------------------------
--- Placeholder table for view `book_usage`.`current_br2`
+-- View `previous_br2`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_usage`.`current_br2` (`book_id` INT, `counter_br2` INT);
+CREATE OR REPLACE VIEW `previous_br2` AS select `counter_br2`.`book_id` AS `book_id`,`counter_br2`.`counter_br2` AS `counter_br2` from `counter_br2` where (`counter_br2`.`usage_year` = 2011);
 
 -- -----------------------------------------------------
--- Placeholder table for view `book_usage`.`overlap`
+-- View `all_usage`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_usage`.`overlap` (`book_id` INT, `platforms` INT);
+CREATE OR REPLACE VIEW `all_usage` AS select `b`.`id` AS `id`,`cbr1`.`counter_br1` AS `current_br1`,`cbr2`.`counter_br2` AS `current_br2`,`pbr1`.`counter_br1` AS `previous_br1`,`pbr2`.`counter_br2` AS `previous_br2` from ((((`books` `b` left join `current_br1` `cbr1` on((`b`.`id` = `cbr1`.`book_id`))) left join `current_br2` `cbr2` on((`b`.`id` = `cbr2`.`book_id`))) left join `previous_br1` `pbr1` on((`b`.`id` = `pbr1`.`book_id`))) left join `previous_br2` `pbr2` on((`b`.`id` = `pbr2`.`book_id`)));
 
 -- -----------------------------------------------------
--- Placeholder table for view `book_usage`.`platforms_vendors`
+-- View `overlap`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_usage`.`platforms_vendors` (`id` INT, `platform_vendor` INT);
+CREATE OR REPLACE VIEW `overlap` AS select `bp`.`book_id` AS `book_id`,concat(`p`.`platform`,' (',`v`.`vendor`,')') AS `platforms` from ((`books_platforms` `bp` left join `platforms` `p` on((`bp`.`platform_id` = `p`.`id`))) left join `vendors` `v` on((`p`.`vendor_id` = `v`.`id`)));
 
 -- -----------------------------------------------------
--- Placeholder table for view `book_usage`.`previous_br1`
+-- View `platforms_vendors`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_usage`.`previous_br1` (`book_id` INT, `counter_br1` INT);
+CREATE OR REPLACE VIEW `platforms_vendors` AS (select `p`.`id` AS `id`,concat(`p`.`platform`,' (',`v`.`vendor`,')') AS `platform_vendor` from (`platforms` `p` left join `vendors` `v` on((`p`.`vendor_id` = `v`.`id`))));
 
 -- -----------------------------------------------------
--- Placeholder table for view `book_usage`.`previous_br2`
+-- View `unicode`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_usage`.`previous_br2` (`book_id` INT, `counter_br2` INT);
-
--- -----------------------------------------------------
--- Placeholder table for view `book_usage`.`unicode`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_usage`.`unicode` (`id` INT, `title` INT, `valid_utf8` INT);
-
--- -----------------------------------------------------
--- View `book_usage`.`all_usage`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `book_usage`.`all_usage`;
-USE `book_usage`;
-CREATE  OR REPLACE VIEW `book_usage`.`all_usage` AS select `b`.`id` AS `id`,`cbr1`.`counter_br1` AS `current_br1`,`cbr2`.`counter_br2` AS `current_br2`,`pbr1`.`counter_br1` AS `previous_br1`,`pbr2`.`counter_br2` AS `previous_br2` from ((((`book_usage`.`books` `b` left join `book_usage`.`current_br1` `cbr1` on((`b`.`id` = `cbr1`.`book_id`))) left join `book_usage`.`current_br2` `cbr2` on((`b`.`id` = `cbr2`.`book_id`))) left join `book_usage`.`previous_br1` `pbr1` on((`b`.`id` = `pbr1`.`book_id`))) left join `book_usage`.`previous_br2` `pbr2` on((`b`.`id` = `pbr2`.`book_id`)));
-
--- -----------------------------------------------------
--- View `book_usage`.`counter_usage`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `book_usage`.`counter_usage`;
-USE `book_usage`;
-CREATE  OR REPLACE VIEW `book_usage`.`counter_usage` AS (select `book_usage`.`counter_br1`.`book_id` AS `book_id`,`book_usage`.`counter_br1`.`vendor_id` AS `vendor_id`,`book_usage`.`counter_br1`.`platform_id` AS `platform_id`,`book_usage`.`counter_br1`.`usage_year` AS `usage_year`,`book_usage`.`counter_br1`.`counter_br1` AS `counter_usage`,'br1' AS `usage_type` from `book_usage`.`counter_br1`) union all (select `book_usage`.`counter_br2`.`book_id` AS `book_id`,`book_usage`.`counter_br2`.`vendor_id` AS `vendor_id`,`book_usage`.`counter_br2`.`platform_id` AS `platform_id`,`book_usage`.`counter_br2`.`usage_year` AS `usage_year`,`book_usage`.`counter_br2`.`counter_br2` AS `counter_usage`,'br2' AS `usage_type` from `book_usage`.`counter_br2`);
-
--- -----------------------------------------------------
--- View `book_usage`.`current_br1`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `book_usage`.`current_br1`;
-USE `book_usage`;
-CREATE  OR REPLACE VIEW `book_usage`.`current_br1` AS select `book_usage`.`counter_br1`.`book_id` AS `book_id`,`book_usage`.`counter_br1`.`counter_br1` AS `counter_br1` from `book_usage`.`counter_br1` where (`book_usage`.`counter_br1`.`usage_year` = 2012);
-
--- -----------------------------------------------------
--- View `book_usage`.`current_br2`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `book_usage`.`current_br2`;
-USE `book_usage`;
-CREATE  OR REPLACE VIEW `book_usage`.`current_br2` AS select `book_usage`.`counter_br2`.`book_id` AS `book_id`,`book_usage`.`counter_br2`.`counter_br2` AS `counter_br2` from `book_usage`.`counter_br2` where (`book_usage`.`counter_br2`.`usage_year` = 2012);
-
--- -----------------------------------------------------
--- View `book_usage`.`overlap`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `book_usage`.`overlap`;
-USE `book_usage`;
-CREATE  OR REPLACE VIEW `book_usage`.`overlap` AS select `bp`.`book_id` AS `book_id`,concat(`p`.`platform`,' (',`v`.`vendor`,')') AS `platforms` from ((`book_usage`.`books_platforms` `bp` left join `book_usage`.`platforms` `p` on((`bp`.`platform_id` = `p`.`id`))) left join `book_usage`.`vendors` `v` on((`p`.`vendor_id` = `v`.`id`)));
-
--- -----------------------------------------------------
--- View `book_usage`.`platforms_vendors`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `book_usage`.`platforms_vendors`;
-USE `book_usage`;
-CREATE  OR REPLACE VIEW `book_usage`.`platforms_vendors` AS (select `p`.`id` AS `id`,concat(`p`.`platform`,' (',`v`.`vendor`,')') AS `platform_vendor` from (`book_usage`.`platforms` `p` left join `book_usage`.`vendors` `v` on((`p`.`vendor_id` = `v`.`id`))));
-
--- -----------------------------------------------------
--- View `book_usage`.`previous_br1`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `book_usage`.`previous_br1`;
-USE `book_usage`;
-CREATE  OR REPLACE VIEW `book_usage`.`previous_br1` AS select `book_usage`.`counter_br1`.`book_id` AS `book_id`,`book_usage`.`counter_br1`.`counter_br1` AS `counter_br1` from `book_usage`.`counter_br1` where (`book_usage`.`counter_br1`.`usage_year` = 2011);
-
--- -----------------------------------------------------
--- View `book_usage`.`previous_br2`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `book_usage`.`previous_br2`;
-USE `book_usage`;
-CREATE  OR REPLACE VIEW `book_usage`.`previous_br2` AS select `book_usage`.`counter_br2`.`book_id` AS `book_id`,`book_usage`.`counter_br2`.`counter_br2` AS `counter_br2` from `book_usage`.`counter_br2` where (`book_usage`.`counter_br2`.`usage_year` = 2011);
-
--- -----------------------------------------------------
--- View `book_usage`.`unicode`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `book_usage`.`unicode`;
-USE `book_usage`;
-CREATE  OR REPLACE VIEW `book_usage`.`unicode` AS select `book_usage`.`books`.`id` AS `id`,`book_usage`.`books`.`title` AS `title`,`book_usage`.`books`.`valid_utf8` AS `valid_utf8` from `book_usage`.`books` where ((length(`book_usage`.`books`.`title`) <> char_length(`book_usage`.`books`.`title`)) and ((`book_usage`.`books`.`valid_utf8` = 'N') or (`book_usage`.`books`.`valid_utf8` = 'Unknown')));
-
+CREATE OR REPLACE VIEW `unicode` AS select `books`.`id` AS `id`,`books`.`title` AS `title`,`books`.`valid_utf8` AS `valid_utf8` from `books` where ((length(`books`.`title`) <> char_length(`books`.`title`)) and ((`books`.`valid_utf8` = 'N') or (`books`.`valid_utf8` = 'Unknown')));
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-```
